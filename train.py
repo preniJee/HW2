b3c4c960511b3eec10371dcff72326274384f1ef
pjee <preniazizian@gmail.com>
2021-10-22 21:36:45 -0700

    files

--- train.py
+++ train.py
@@ -9,37 +9,52 @@ from classify import classify
 from sklearn.metrics import f1_score, mean_squared_error, classification_report
 from sklearn.preprocessing import MultiLabelBinarizer
 
-def train(train_data_path, l_rate, n_epoch, save_path, validation_data_path, val_result_path):
-    # network=NNComp(feature_dim=args.f,h_units=args.u,output_dim=n_outputs)
-    learning_rate = l_rate
-    with open(train_data_path, "rb") as f:
-        X_train, y_train = pkl.load(f)
-        ### Flatten the embeddings to 1d array ###
-        X_train = np.asarray([np.asarray(row).flatten() for row in X_train])
 
-    with open(validation_data_path, "rb") as f:
-        X_val, y_val = pkl.load(f)
-        X_val = np.asarray([np.asarray(row).flatten() for row in X_val])
+def train(hidden_units, train_data_path, l_rate, n_epoch, save_path,max_seq_length=None):
+
+    f1 = Features(train_data_path, stopwords_file="stopwords.txt")
+    # print(f1.avg_len, f1.stdv_len)
+    if max_seq_length is None:
+        ### setting the max sequence length based on the dataset features and experimetnal setup
+        if "odia" or "questions" in train_data_path:
+            max_seq_length=f1.max_len
+        elif  "product" in  train_data_path:
+            max_seq_length = int(f1.avg_len + f1.stdv_len)
+        else :
+            max_seq_length=60
+        # print(max_seq_length)
+
+    X_train= f1.get_features(preprocessed=f1.preprocessed_text, max_seq_length=max_seq_length)
+    y_train= f1.numeric_classes
+    learning_rate = l_rate
+    # with open(train_data_path, "rb") as f:
+    #     X_train, y_train = pkl.load(f)
+    #     ### Flatten the embeddings to 1d array ###
+    #     X_train = np.asarray([np.asarray(row).flatten() for row in X_train])
+    #
+    # with open(validation_data_path, "rb") as f:
+    #     X_val, y_val = pkl.load(f)
+    #     X_val = np.asarray([np.asarray(row).flatten() for row in X_val])
 
     n_inputs = len(X_train[0])
     n_outputs = len(set(y_train))
 
-    model = NNComp(feature_dim=n_inputs, h_units=2, output_dim=n_outputs)
+    model = NNComp(feature_dim=n_inputs, h_units=hidden_units, output_dim=n_outputs)
     print("Start training...")
 
-    print("Shape of training data", np.shape(X_train))
-    print("Shape of test data",np.shape(X_val))
-    val_results = open(val_result_path, "w")
+    # print("Shape of training data", np.shape(X_train))
+    # print("Shape of test data", np.shape(X_val))
+    # val_results = open(val_result_path, "w")
 
     for epoch in range(n_epoch):
-        y_true=[]
-        y_pred=[]
+        y_true = []
+        y_pred = []
         sum_error = 0
         # if epoch%15==0:
         #     learning_rate = step_decay(learning_rate, epoch)
         for i, row in enumerate(X_train):
-            if i%2000==0:
-                print("Sample %d" %(i))
+            if i % 2000 == 0:
+                print("Sample %d" % (i))
             outputs = model.forward(row)
             # t_pred = [0 for _ in range(n_outputs)]
             # t_pred[np.argmax(outputs)] = 1
@@ -48,19 +63,18 @@ def train(train_data_path, l_rate, n_epoch, save_path, validation_data_path, val
             expected[y_train[i]] = 1
             y_true.append(expected)
 
-
             sum_error += sum([(expected[i] - outputs[i]) ** 2 for i in range(len(expected))])
             model.backward(expected)
             model.update_weights(row, learning_rate)
 
         f1 = f1_score(y_true=y_train, y_pred=y_pred, average="macro")
-        print('Train log : epoch=%d, lrate=%.3f, error=%.3f, f1=%.3f' % (epoch, learning_rate, sum_error,f1))
+        print('Train log : epoch=%d, lrate=%.3f, error=%.3f, f1=%.3f' % (epoch, learning_rate, sum_error, f1))
         ### validate on validation set and print the loss on val set and f1 score on val set ###
-        print("Start validating...")
-        validate(X_val, y_val, model, epoch, val_results)
+        # print("Start validating...")
+        # validate(X_val, y_val, model, epoch, val_results)
 
     ### save the model ##
-    val_results.close()
+    # val_results.close()
     model.save_model(save_path)
 
 
@@ -118,20 +132,20 @@ def step_decay(rate, epoch):
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description='Neural net training arguments.')
-    #
-    # parser.add_argument('-u', type=int, help='number of hidden units')
-    # parser.add_argument('-l', type=float, help='learning rate')
-    # parser.add_argument('-f', type=int, help='max sequence length')
-    # parser.add_argument('-b', type=int, help='mini-batch size')
-    # parser.add_argument('-e', type=int, help='number of epochs to train for')
-    # parser.add_argument('-E', type=str, help='word embedding file')
-    # parser.add_argument('-i', type=str, help='training file')
-    # parser.add_argument('-o', type=str, help='model file to be written')
-    #
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Neural net training arguments.')
 
-    # train(train_data_path=args.i,l_rate=args.l,n_epoch=args.e,save_path=args.o)
+    parser.add_argument('-u', type=int, help='number of hidden units')
+    parser.add_argument('-l', type=float, help='learning rate')
+    parser.add_argument('-f', type=int, help='max sequence length')
+    parser.add_argument('-b', type=int, help='mini-batch size')
+    parser.add_argument('-e', type=int, help='number of epochs to train for')
+    parser.add_argument('-E', type=str, help='word embedding file')
+    parser.add_argument('-i', type=str, help='training file')
+    parser.add_argument('-o', type=str, help='model file to be written')
+
+    args = parser.parse_args()
+
+    train(hidden_units=args.u,train_data_path=args.i, l_rate=args.l, n_epoch=args.e, save_path=args.o,max_seq_length=args.f)
 
     # f1 = Features(data_file="datasets/4dim.train.txt", stopwords_file="stopwords.txt")
     # X_train = f1.get_features("4dim", f1.preprocessed_text,40)
@@ -145,5 +159,5 @@ if __name__ == '__main__':
     # n_inputs = len(X_train[0])
     # n_outputs = len(set(y_train))
     # network = NNComp(feature_dim=n_inputs, h_units=2, output_dim=n_outputs)
-    train(train_data_path="products_train.pkl", l_rate=0.3, n_epoch=100, save_path="products_V0.model",
-          validation_data_path="products_test.pkl", val_result_path="products_val_results.txt")
+    # train(train_data_path="products_train.pkl", l_rate=0.3, n_epoch=100, save_path="products_V0.model",
+    #       validation_data_path="products_test.pkl", val_result_path="products_val_results.txt")
